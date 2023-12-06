0636d2a929b05e229e8264b2f80f0465a98d2c40
cozypet <yanhan860711@gmail.com>
2023-12-06 11:12:15 +0100

    update regulation

--- regulationAI.py
+++ regulationAI.py
@@ -1,104 +1,136 @@
-import requests
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
 from bs4 import BeautifulSoup
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from numpy.linalg import norm
+import requests
 import openai
 import pymongo
 import os
 import json
+import numpy as np
+import pandas as pd
 
+# Set OpenAI API Key
+openai.api_key = os.environ['OPENAI_API_KEY']
 
-
+# Define the URL
 url = "https://en.wikipedia.org/wiki/Markets_in_Financial_Instruments_Directive_2014#Directive_2014/65/EU_/_Regulation_(EU)_No_600/2014"
-response = requests.get(url)
 
-soup = BeautifulSoup(response.content, 'html.parser')
-
-# find all the text on the page
-text = soup.get_text()
-
-# find the content div
-content_div = soup.find('div', {'class': 'mw-parser-output'})
-
-# remove unwanted elements from div
-unwanted_tags = ['sup', 'span', 'table', 'ul', 'ol']
-for tag in unwanted_tags:
-    for match in content_div.findAll(tag):
-        match.extract()
+# Connect to MongoDB server
 
 
-print(content_div.get_text())
-article_text = content_div.get_text()
+def connect_mongodb():
+    mongo_url = "mongodb+srv://han:han@cluster0.bofm7.mongodb.net/test?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(mongo_url)
+    db = client["AIRegulation"]
+    collection = db["mifid2"]
+    return collection
 
 
-text_splitter = RecursiveCharacterTextSplitter(
-    # Set a really small chunk size, just to show.
-    chunk_size = 100,
-    chunk_overlap  = 20,
-    length_function = len,
-)
+def get_embedding(text, model="text-embedding-ada-002"):
+    text = text.replace("\n", " ")
+    return openai.Embedding.create(input=[text], model=model)['data'][0]['embedding']
 
 
-texts = text_splitter.create_documents([article_text])
-# Initialize an empty list to store the JSON objects
-json_objects = []
+def data_prep():
+    # Load data
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    text = soup.get_text()
+    content_div = soup.find('div', {'class': 'mw-parser-output'})
 
-# Loop through each text in the texts list
-for idx, document in enumerate(texts):
-    # Convert the Document object to a string
-    text = str(document)
-    
-    # Create a JSON object with field "mifid2text" and value as the current text
-    json_obj = {
-        "mifid2text": text
-    }
-    # Append the JSON object to the list
-    json_objects.append(json_obj)
+    # Remove unwanted elements from div
+    unwanted_tags = ['sup', 'span', 'table', 'ul', 'ol']
+    for tag in unwanted_tags:
+        for match in content_div.findAll(tag):
+            match.extract()
+    article_text = content_div.get_text()
 
-# Define the filename for the JSON file
-json_filename = "mifid2_texts.json"
+    # Split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=100,
+        chunk_overlap=20,
+        separators=["\n\n", "\n", "(?<=\. )", " ", ""],
+        length_function=len,
+    )
+    texts = text_splitter.create_documents([article_text])
 
-# Write the list of JSON objects to a JSON file
-with open(json_filename, "w") as json_file:
-    json.dump(json_objects, json_file, indent=4)
+    # Calculate embeddings
+    text_chunks = [text.page_content for text in texts]
+    df = pd.DataFrame({'text_chunks': text_chunks})
+    df['ada_embedding'] = df.text_chunks.apply(
+        lambda x: get_embedding(x, model='text-embedding-ada-002'))
 
-print(f"JSON objects stored in {json_filename}")
+    # Store into MongoDB
+    collection = connect_mongodb()
+    df_dict = df.to_dict(orient='records')
+    collection.insert_many(df_dict)
 
-
-# MongoDB Atlas configuration
-mongo_url = "mongodb+srv://yourusername:psw@yourcluster/test?retryWrites=true&w=majority"
-client = pymongo.MongoClient(mongo_url)
-db = client["AIRegulation"]
-collection = db["mifid2"]
-
-# Load the JSON data from the file
-with open(json_filename, "r") as json_file:
-    data = json.load(json_file)
-
-# Insert the data into the collection
-#collection.insert_many(data)
-
-#print("Data inserted into MongoDB Atlas")
-
-#with open('mifid2withoutembeddings.json', 'w') as json_file:
-
-#    json.dump(texts, json_file, indent=4)
-#print(texts[0])
-#print(texts[1])
-
-# Generate embeddings for only text[0]
-url = "https://api.openai.com/v1/embeddings"
-openai_key = "YOUR OPENAI KEY"  # Replace with your OpenAI key.
+    print("Data loaded successfully")
 
 
-# for text in texts:
-#     response = requests.post(
-#         url,
-#         json={"input": text.page_content, "model": "text-embedding-ada-002"},
-#         headers={
-#             "Authorization": f"Bearer {openai_key}",
-#             "Content-Type": "application/json",
-#         },
-#     )
-#     embedding=response.json()["data"][0]["embedding"]
+def find_similar_documents(embedding):
+    collection = connect_mongodb()
+    documents = list(
+        collection.aggregate(
+            [
+                {
+                    "$search": {
+                        "index": "default",
+                        "knnBeta": {
+                            "vector": embedding,
+                            "path": "ada_embedding",
+                            "k": 10,
+                        },
+                    }
+                },
+                {"$project": {"_id": 0,"text_chunks":1}}
+            ]))
+    return documents
 
-#     print(embedding)
+
+def qna(users_question):
+    llm = OpenAI(temperature=1)
+    question_embedding = get_embedding(
+        text=users_question, model="text-embedding-ada-002")
+
+    # print(users_question)
+
+    context = ""
+    documents = find_similar_documents(question_embedding)
+    for document in documents:
+            print(str(document) + "\n")
+    print(documents)
+    df = pd.DataFrame(documents)
+    for index, row in df[0:10].iterrows():
+        context = context + " " + row.text_chunks
+
+    template = """
+    You are a chat bot who loves to help people! Given the following context sections, answer the
+    question using only the given context. If you are unsure and the answer is not
+    explicitly written in the documentation, say "Sorry, I don't know how to help with that."
+
+    Context sections:
+    {context}
+
+    Question:
+    {users_question}
+
+    Answer:
+    """
+
+    prompt = PromptTemplate(template=template, input_variables=[
+                            "context", "users_question"])
+
+    prompt_text = prompt.format(context=context, users_question=users_question)
+
+    response = llm(prompt_text)
+
+    print(response)
+
+
+while True:
+    # data_prep()
+    users_input = input("What do you want to know about Mifid?\n")
+    qna(users_input)
